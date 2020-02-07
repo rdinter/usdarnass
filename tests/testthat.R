5d1788c9b0f850f764b54d30afea7b11699a8f35
Robert Dinterman <robert.dinterman@gmail.com>
2020-02-07 14:13:06 -0500

    local tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(usdarnass)
+
+test_check("usdarnass")
---
+++ tests/testthat/test-local.R
@@ -0,0 +1,32 @@
+skip_on_cran()
+
+# ---- returns the correct type of value ----------------------------------
+
+test_that("nass_count returns how many", {
+  x <- nass_count()
+  
+  expect_is(x, "numeric")
+})
+
+test_that("nass_param returns what of", {
+  x <- nass_param("source_desc")
+  
+  expect_is(x, "character")
+})
+
+test_that("nass_data returns what", {
+  x <- nass_data(year = 2012,
+                 short_desc = "AG LAND, INCL BUILDINGS - ASSET VALUE, MEASURED IN $",
+                 county_name = "WAKE",
+                 state_name = "NORTH CAROLINA")
+  
+  expect_is(x, "data.frame")
+})
+
+
+# ---- parameter issues ---------------------------------------------------
+# 
+# test_that("year doesn't accept logicals", {
+#   expect_error(nass_count(year = >2012))
+# })
+# 
